c167d7f1ce5e6dbd8466f7a0df2ee6d27a7cc162
airiaddy <airiaddy@live.com>
2016-02-28 22:29:54 +0700

    Added files via upload

---
+++ script.R
@@ -0,0 +1,5 @@
+raw.train <- read.csv("/home/iftikar/workspace/kaggle_titanic/input/train.csv", header = TRUE)
+raw.test <- read.csv("/home/iftikar/workspace/kaggle_titanic/input/test.csv", header = TRUE)
+
+dataset.train <- model.frame(~ Survived+Pclass+Sex+Age+SibSp+Parch+Fare+Embarked, data = raw.train)
+dataset.test <- model.frame(~ Pclass+Sex+Age+SibSp+Parch+Fare+Embarked, data = raw.test)
